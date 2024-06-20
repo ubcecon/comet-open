7240c192cede45f1bb7ba14a8e11c350a4768e2f
Luiz Ramos <lframos@student.ubc.ca>
2024-06-20 11:01:28 -0700

    DONE - getting_started_intro_to_jupyter

--- Getting_started/getting_started_intro_to_jupyter/getting_started_intro_to_jupyter_tests.r
+++ Getting_started/getting_started_intro_to_jupyter/getting_started_intro_to_jupyter_tests.r
@@ -19,15 +19,14 @@ test_2 <- function() {
 
 test_3 <- function() {
   test_that("Solution is incorrect", {
-    expect_equal(digest(answer_3), "c2b1646c9d0d86b49379e998e940e772")
+    expect_equal(digest(answer_3), "75f1160e72554f4270c809f041c7a776")
   })
   print("Success!")
 }
 
-
 test_4 <- function() {
   test_that("Solution is incorrect", {
-    expect_equal(digest(answer_4), "75f1160e72554f4270c809f041c7a776")
+    expect_equal(digest(answer_4), "c2b1646c9d0d86b49379e998e940e772")
   })
   print("Success!")
 }
