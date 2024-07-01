85e37f8c678d2cb5b6342ebb192a51d9c227c3ea
Luiz Ramos <lframos@student.ubc.ca>
2024-07-01 11:04:40 -0700

    Self tests edits

--- Getting_started/getting_started_intro_to_jupyter/getting_started_intro_to_jupyter_tests.r
+++ Getting_started/getting_started_intro_to_jupyter/getting_started_intro_to_jupyter_tests.r
@@ -1,5 +1,3 @@
-print("You did it! Way to go!")
-
 library(testthat)
 library(digest)
 
@@ -21,15 +19,14 @@ test_2 <- function() {
 
 test_3 <- function() {
   test_that("Solution is incorrect", {
-    expect_equal(digest(answer_3), "c2b1646c9d0d86b49379e998e940e772")
+    expect_equal(digest(answer_3), "75f1160e72554f4270c809f041c7a776")
   })
   print("Success!")
 }
 
-
 test_4 <- function() {
   test_that("Solution is incorrect", {
-    expect_equal(digest(answer_4), "127a2ec00989b9f7faf671ed470be7f8")
+    expect_equal(digest(answer_4), "c2b1646c9d0d86b49379e998e940e772")
   })
   print("Success!")
 }
